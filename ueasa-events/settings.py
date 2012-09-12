fafe7018c6d9189d034e067de84e0621dafa393d
Ben Summers <bensums@gmail.com>
2012-09-12 18:05:24 +0100

    installed south

--- ueasa-events/settings.py
+++ ueasa-events/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'social_auth',
     'events',
     'storages',
+    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
