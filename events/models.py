bc6f22624892c09a14063e0585821bfed7eefc99
Ben Summers <bensums@gmail.com>
2012-09-12 17:08:44 +0100

    unicode for Event model

--- events/models.py
+++ events/models.py
@@ -5,3 +5,8 @@ class Event(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateTimeField('Event time')
     people = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.name
+
+
