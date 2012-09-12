7bc4d6d0130cddfb10c35a64ffbc38001fea261a
Ben Summers <bensums@gmail.com>
2012-09-12 14:32:57 +0100

    enable admin

--- ueasa-events/settings.py
+++ ueasa-events/settings.py
@@ -114,7 +114,7 @@ INSTALLED_APPS = (
     'social_auth',
     'events',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
