9af44fa82bc8c1d98eaebf66451be70cd1bcac58
Ben Summers <bensums@gmail.com>
2012-09-12 14:10:47 +0100

    made app for diving events

---
+++ events/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField('Event time')
+    people = models.ManyToManyField(User)
--- ueasa-events/settings.py
+++ ueasa-events/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'gunicorn',
     'social_auth',
+    'events',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
