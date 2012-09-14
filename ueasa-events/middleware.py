9fbea37eab3650d082807797ee82d3e28592eddf
Ben Summers <bensums@gmail.com>
2012-09-14 10:04:07 +0100

    tidied facebook csrf middleware

--- ueasa-events/middleware.py
+++ ueasa-events/middleware.py
@@ -6,16 +6,18 @@ import hashlib
 from django.conf import settings
 
 class IgnoreFbCsrfMiddleware(object):
-    """From http://djangosnippets.org/snippets/2272/
-       See https://developers.facebook.com/docs/canvas/post/
-       """
+    """
+    From http://djangosnippets.org/snippets/2272/
+    See https://developers.facebook.com/docs/canvas/post/
+    """
 
     def process_request(self, request):
         signed_request = request.REQUEST.get('signed_request', None)
         # If no signed_request then do nothing.
         if signed_request == None:
             return
-        signed_request = fb_decode_signed_request(signed_request, settings.FACEBOOK_API_SECRET)
+        signed_request = fb_decode_signed_request(signed_request,
+                settings.FACEBOOK_API_SECRET)
         request.csrf_processing_done = signed_request != None
 
 
@@ -26,7 +28,10 @@ def b64url_decode(encoded):
     return base64.urlsafe_b64decode(padding_fixed)
 
 def fb_decode_signed_request(signed_request, app_secret):
-    """Decode Facebook's signed_request. Fail if signature cannot be verified since this indicates the request likely does not originate from Facebook."""
+    """
+    Decode Facebook's signed_request. Fail if signature cannot be verified
+    since this indicates the request likely does not originate from Facebook.
+    """
     sig, payload = str(signed_request).split('.', 1)
 
     data = json.loads(b64url_decode(payload))
@@ -39,8 +44,6 @@ def fb_decode_signed_request(signed_request, app_secret):
     expected_sig = base64.urlsafe_b64encode(h.digest()).replace('=', '')
 
     if sig != expected_sig:
-        print 'sig: ' + sig
-        print 'expected_sig: ' + expected_sig
         raise ValueError('bad signature')
 
     return data
