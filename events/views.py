05548a0a11631d6fe0cb58a821ff682bca030140
Ben Summers <bensums@gmail.com>
2012-09-18 13:28:42 +0100

    See My Events on homepage

--- events/views.py
+++ events/views.py
@@ -4,10 +4,16 @@ from events.models import Event
 
 def home(request):
     latest_events = Event.objects.all().order_by('-post_date')[:5]
-    #my_events = Events.
+    if request.user.is_authenticated():
+        my_events = request.user.event_set.all()
+    else:
+        my_events = None
     return render_to_response('events/home.html',
-            {'latest_events': latest_events}
-    )
+            {
+                'latest_events': latest_events,
+                'my_events': my_events
+            }
+        )
 
 
 def detail(request, slug):
