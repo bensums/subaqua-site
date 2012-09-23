1f3aacacd8abe18d6038e0a8cf98137c15bc77d2
Ben Summers <bensums@gmail.com>
2012-09-23 23:34:01 +0100

    Added end time field. Renamed date field.

--- events/models.py
+++ events/models.py
@@ -5,7 +5,8 @@ class Event(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=210)
     post_date = models.DateTimeField('Post time', auto_now_add=True, null=True)
-    date = models.DateTimeField('Event time')
+    start_time = models.DateTimeField('Start time')
+    end_time = models.DateTimeField('End time', null=True, blank=True)
     people = models.ManyToManyField(User, null=True, blank=True)
 
     def __unicode__(self):
