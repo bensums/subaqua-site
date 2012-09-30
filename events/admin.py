9f66c203555114e1849a46851345c970afe4cdbb
Ben Summers <bensums@gmail.com>
2012-09-30 15:24:57 +0100

    Add RSVP inline to event admin

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from events.models import Event, EventCategory
+from events.models import Event, EventCategory, RSVP
+
+class RSVPInline(admin.TabularInline):
+    model = RSVP
+    extra = 0
 
 class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
-
+    inlines = (RSVPInline,)
 
 class EventCategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
